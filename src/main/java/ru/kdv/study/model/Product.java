7ef732b403f38b51ee582a3a09d140bca661c24b
Kiryakov Denis <KiryakovDenis@gmail.com>
2025-03-14 15:26:20 +0500

    Допилил gitignore и добавил интерфейс Serializable к модели Product

--- src/main/java/ru/kdv/study/model/Product.java
+++ src/main/java/ru/kdv/study/model/Product.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 @Builder
-public class Product {
+public class Product implements Serializable {
     Long id;
     String name;
     BigDecimal price;
